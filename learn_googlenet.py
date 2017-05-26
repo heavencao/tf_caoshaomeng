76e075c54da7a41513ceaf55c0439ff036344fae
heavencao <18039293629@163.com>
2017-05-26 13:19:47 +0800

    Create learn_googlenet.py

---
+++ learn_googlenet.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+""" Convolutional network applied to CIFAR-10 dataset classification task.
+References:
+    Learning Multiple Layers of Features from Tiny Images, A. Krizhevsky, 2009.
+Links:
+    [CIFAR-10 Dataset](https://www.cs.toronto.edu/~kriz/cifar.html)
+"""
+
+from __future__ import division, print_function, absolute_import
+import tflearn
+from tflearn.data_utils import shuffle, to_categorical
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+import pickle,os
+import tflearn
+import gzip
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d, avg_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.merge_ops import merge
+from tflearn.layers.estimator import regression
+import input_datas
+
+X, Y = input_datas.data_sets()
+trainx=X[:2200]
+trainy=Y[:2200]
+testx=X[2200:]
+testy=Y[2200:]
+
+network = input_data(shape=[None, 440, 440, 1])
+conv1_7_7 = conv_2d(network, 64, 7, strides=2, activation='relu', name = 'conv1_7_7_s2')
+pool1_3_3 = max_pool_2d(conv1_7_7, 3,strides=2)
+pool1_3_3 = local_response_normalization(pool1_3_3)
+conv2_3_3_reduce = conv_2d(pool1_3_3, 64,1, activation='relu',name = 'conv2_3_3_reduce')
+conv2_3_3 = conv_2d(conv2_3_3_reduce, 192,3, activation='relu', name='conv2_3_3')
+conv2_3_3 = local_response_normalization(conv2_3_3)
+pool2_3_3 = max_pool_2d(conv2_3_3, kernel_size=3, strides=2, name='pool2_3_3_s2')
+inception_3a_1_1 = conv_2d(pool2_3_3, 64, 1, activation='relu', name='inception_3a_1_1')
+inception_3a_3_3_reduce = conv_2d(pool2_3_3, 96,1, activation='relu', name='inception_3a_3_3_reduce')
+inception_3a_3_3 = conv_2d(inception_3a_3_3_reduce, 128,filter_size=3,  activation='relu', name = 'inception_3a_3_3')
+inception_3a_5_5_reduce = conv_2d(pool2_3_3,16, filter_size=1,activation='relu', name ='inception_3a_5_5_reduce' )
+inception_3a_5_5 = conv_2d(inception_3a_5_5_reduce, 32, filter_size=5, activation='relu', name= 'inception_3a_5_5')
+inception_3a_pool = max_pool_2d(pool2_3_3, kernel_size=3, strides=1, )
+inception_3a_pool_1_1 = conv_2d(inception_3a_pool, 32, filter_size=1, activation='relu', name='inception_3a_pool_1_1')
+
+# merge the inception_3a__
+inception_3a_output = merge([inception_3a_1_1, inception_3a_3_3, inception_3a_5_5, inception_3a_pool_1_1], mode='concat', axis=3)
+
+
+pool5_7_7 = avg_pool_2d(inception_3a_output, kernel_size=7, strides=1)
+pool5_7_7 = dropout(pool5_7_7, 0.4)
+loss = fully_connected(pool5_7_7, 19,activation='softmax')
+network = regression(loss, optimizer='momentum',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+model = tflearn.DNN(network, tensorboard_verbose=0)
+
+model.load("./cnn_model/googlenet_4model.tfl")
+model.fit(trainx, trainy, n_epoch=100, shuffle=True, validation_set=(testx,testy),
+show_metric=True, batch_size=10, run_id='googlenet')
+
+model.save("./cnn_model/googlenet_4model.tfl")
